2083e889d9365f33ea7f350c516085ca26dec857
Bachir Khiati <bachir.khiati@gmail.com>
2019-04-28 15:40:45 +0300

    android

--- android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollPackage.java
+++ android/src/main/java/com/reactnativecommunity/cameraroll/CameraRollPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactnativecommunity.cameraroll;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,19 +10,20 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNCameraRollMediaPackage implements ReactPackage {
+
+public class CameraRollPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNCameraRollMediaModule(reactContext));
+        return Arrays.<NativeModule>asList(new CameraRollModule(reactContext));
     }
 
     // Deprecated from RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
